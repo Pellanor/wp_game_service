805b28dfa58e535690a1ba2da3e1eac69517a029
Pellanor <pellanor@gmail.com>
2016-09-19 21:53:14 -0600

    Flask app that works on windows

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 INTERP = os.path.expanduser('~/python.valsarn.com/venv/bin/python')
 if sys.executable != INTERP:
-	os.execl(INTERP, INTERP, *sys.argv)
+    os.execl(INTERP, INTERP, *sys.argv)
 
 # append current dir to module path
 cwd = os.getcwd()
@@ -11,14 +11,13 @@ sys.path.append(cwd)
 sys.path.append(cwd + 'wp_game')
 sys.path.append(cwd + '/venv/bin')
 
-
 from flask import Flask
+
 application = Flask(__name__)
+
 
 @application.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
-#from wp_game.app import app as application
-
+# from wp_game.app import app as application
